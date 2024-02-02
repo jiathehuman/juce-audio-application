2c342d6a95c2fffda4daaa025d9ce17d10662837
jiathehuman <jiathehuman@gmail.com>
2024-02-02 10:19:43 +0800

    finished filter

--- Source/custom_style.cpp
+++ Source/custom_style.cpp
@@ -85,6 +85,110 @@ void CustomStyle::drawRotarySlider (Graphics& g, int x, int y, int width, int he
     g.drawLine(backgroundArc.getBounds().getCentreX(), backgroundArc.getBounds().getCentreY(),thumbPoint.getX(),thumbPoint.getY(),lineW);
     // End of new code ----------------------------------------
 }
+
+void CustomStyle::drawLinearSlider (Graphics& g, int x, int y, int width, int height,
+                                       float sliderPos,
+                                       float minSliderPos,
+                                       float maxSliderPos,
+                                       const Slider::SliderStyle style, Slider& slider)
+{
+    if (slider.isBar())
+    {
+        g.setColour (slider.findColour (Slider::trackColourId));
+        g.fillRect (slider.isHorizontal() ? Rectangle<float> (static_cast<float> (x), (float) y + 0.5f, sliderPos - (float) x, (float) height - 1.0f)
+                                          : Rectangle<float> ((float) x + 0.5f, sliderPos, (float) width - 1.0f, (float) y + ((float) height - sliderPos)));
+
+        drawLinearSliderOutline (g, x, y, width, height, style, slider);
+    }
+    else
+    {
+        auto isTwoVal   = (style == Slider::SliderStyle::TwoValueVertical   || style == Slider::SliderStyle::TwoValueHorizontal);
+        auto isThreeVal = (style == Slider::SliderStyle::ThreeValueVertical || style == Slider::SliderStyle::ThreeValueHorizontal);
+
+//        auto trackWidth = jmin (6.0f, slider.isHorizontal() ? (float) height * 0.25f : (float) width * 0.25f);
+        auto trackWidth = jmin (6.0f, slider.isHorizontal() ? (float) height * 0.25f : (float) width * 2.5f);
+
+
+        Point<float> startPoint (slider.isHorizontal() ? (float) x : (float) x + (float) width * 0.5f,
+                                 slider.isHorizontal() ? (float) y + (float) height * 0.5f : (float) (height + y));
+
+        Point<float> endPoint (slider.isHorizontal() ? (float) (width + x) : startPoint.x,
+                               slider.isHorizontal() ? startPoint.y : (float) y);
+
+        Path backgroundTrack;
+        backgroundTrack.startNewSubPath (startPoint);
+        backgroundTrack.lineTo (endPoint);
+//        g.setColour (slider.findColour (Slider::backgroundColourId));
+        g.setColour (juce::Colour(46, 153, 155));
+        g.strokePath (backgroundTrack, { trackWidth, PathStrokeType::curved, PathStrokeType::rounded });
+
+        Path valueTrack;
+        Point<float> minPoint, maxPoint, thumbPoint;
+
+        if (isTwoVal || isThreeVal)
+        {
+            minPoint = { slider.isHorizontal() ? minSliderPos : (float) width * 0.5f,
+                         slider.isHorizontal() ? (float) height * 0.5f : minSliderPos };
+
+            if (isThreeVal)
+                thumbPoint = { slider.isHorizontal() ? sliderPos : (float) width * 0.5f,
+                               slider.isHorizontal() ? (float) height * 0.5f : sliderPos };
+
+            maxPoint = { slider.isHorizontal() ? maxSliderPos : (float) width * 0.5f,
+                         slider.isHorizontal() ? (float) height * 0.5f : maxSliderPos };
+        }
+        else
+        {
+            auto kx = slider.isHorizontal() ? sliderPos : ((float) x + (float) width * 0.5f);
+            auto ky = slider.isHorizontal() ? ((float) y + (float) height * 0.5f) : sliderPos;
+
+            minPoint = startPoint;
+            maxPoint = { kx, ky };
+        }
+
+        auto thumbWidth = getSliderThumbRadius (slider);
+
+        valueTrack.startNewSubPath (minPoint);
+        valueTrack.lineTo (isThreeVal ? thumbPoint : maxPoint);
+        g.setColour (slider.findColour (Slider::trackColourId));
+        g.strokePath (valueTrack, { trackWidth, PathStrokeType::curved, PathStrokeType::rounded });
+
+        if (! isTwoVal)
+        {
+            g.setColour (slider.findColour (Slider::thumbColourId));
+            g.fillEllipse (Rectangle<float> (static_cast<float> (thumbWidth), static_cast<float> (thumbWidth)).withCentre (isThreeVal ? thumbPoint : maxPoint));
+        }
+
+        if (isTwoVal || isThreeVal)
+        {
+            auto sr = jmin (trackWidth, (slider.isHorizontal() ? (float) height : (float) width) * 0.4f);
+            auto pointerColour = slider.findColour (Slider::thumbColourId);
+
+            if (slider.isHorizontal())
+            {
+                drawPointer (g, minSliderPos - sr,
+                             jmax (0.0f, (float) y + (float) height * 0.5f - trackWidth * 2.0f),
+                             trackWidth * 2.0f, pointerColour, 2);
+
+                drawPointer (g, maxSliderPos - trackWidth,
+                             jmin ((float) (y + height) - trackWidth * 2.0f, (float) y + (float) height * 0.5f),
+                             trackWidth * 2.0f, pointerColour, 4);
+            }
+            else
+            {
+                drawPointer (g, jmax (0.0f, (float) x + (float) width * 0.5f - trackWidth * 2.0f),
+                             minSliderPos - trackWidth,
+                             trackWidth * 2.0f, pointerColour, 1);
+
+                drawPointer (g, jmin ((float) (x + width) - trackWidth * 2.0f, (float) x + (float) width * 0.5f), maxSliderPos - sr,
+                             trackWidth * 2.0f, pointerColour, 3);
+            }
+        }
+
+        if (slider.isBar())
+            drawLinearSliderOutline (g, x, y, width, height, style, slider);
+    }
+}
 CustomRotarySlider::CustomRotarySlider()
 {
     setSliderStyle(Slider::SliderStyle::RotaryHorizontalDrag);
@@ -107,7 +211,8 @@ CustomVerticalSlider::CustomVerticalSlider()
     setSliderStyle(Slider::SliderStyle::LinearBarVertical);
     setTextBoxStyle(Slider::TextBoxBelow, false, 100, 25);
     setColour(Slider::ColourIds::textBoxOutlineColourId, Colours::transparentBlack);
-    setColour(Slider::ColourIds::thumbColourId, juce::Colour(87, 204, 153));
+    setColour(Slider::ColourIds::thumbColourId, juce::Colour(128, 237, 153));
+    setColour(Slider::ColourIds::trackColourId, juce::Colour(242, 212, 146));
     setNumDecimalPlacesToDisplay(3);
     
 };
--- Source/filterClass.cpp
+++ Source/filterClass.cpp
@@ -11,53 +11,20 @@
 #include <JuceHeader.h>
 #include "filterClass.h"
 
+// https://forum.juce.com/t/iir-filter-control-and-addition/6652
+// https://www.youtube.com/watch?v=xJCt0MVZZ7Q&t=1333s to figure out low and highpass
 //==============================================================================
-//filterClass::filterClass()
-//{
-//    // In your constructor, you should add any child components, and
-//    // initialise any special settings that your component needs.
-//
-//}
-//
-//filterClass::~filterClass()
-//{
-//}
-
-//void filterClass::paint (juce::Graphics& g)
-//{
-//    /* This demo code just fills the component's background and
-//       draws some placeholder text to get you started.
-//
-//       You should replace everything in this method with your own
-//       drawing code..
-//    */
-//
-//    g.fillAll (getLookAndFeel().findColour (juce::ResizableWindow::backgroundColourId));   // clear the background
-//
-//    g.setColour (juce::Colours::grey);
-//    g.drawRect (getLocalBounds(), 1);   // draw an outline around the component
-//
-//    g.setColour (juce::Colours::white);
-//    g.setFont (14.0f);
-//    g.drawText ("filterClass", getLocalBounds(),
-//                juce::Justification::centred, true);   // draw some placeholder text
-//}
-//
-//void filterClass::resized()
-//{
-//    // This method is where you should set the bounds of any child
-//    // components that your component contains..
-//
-//}
 
 void FilterClass::setHighpass(bool highpass)
 {
     this -> highpass = highpass;
 }
+
 void FilterClass::setCutoffFreq(float cutoff_frequency)
 {
     this -> cutoff_frequency = cutoff_frequency;
 }
+
 void FilterClass::setSamplingRate(float sampling_rate)
 {
     this -> sampling_rate = sampling_rate;
@@ -86,8 +53,10 @@ void FilterClass::processBlock(juce::AudioBuffer<float>& buffer, juce::MidiBuffe
             const auto inputSample = channelSamples[i];
             // all pass filtering
             const auto all_filtered_sample = al * inputSample + dn_buffer[channel];
+            
             // final filtering, scaling by 0.5 to stay in range
             const auto filterOutput = 0.5f * (inputSample + sign * all_filtered_sample);
+            
             // to output
             channelSamples[i] = filterOutput;
         }
